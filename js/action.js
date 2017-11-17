35b3aaa338ec32c04b4c3a7eeca0ca7bd205e0d7
Deepak Bhaskaran <deepak.ny007@gmail.com>
2017-11-16 22:15:32 -0500

    Added a timer and also updated content on the pages

--- js/action.js
+++ js/action.js
@@ -3,3 +3,19 @@
          $(this).children('ul').toggle('slow');
    });
    });
+
+var myModule = angular.module("timerApp", []);
+myModule.controller("counterCtrl",['$scope','$timeout', function($scope,$timeout){
+$scope.counter = 1380;
+var stopped;
+$scope.countdown = function() {
+   stopped = $timeout(function() {
+      console.log($scope.counter);
+    $scope.counter--;
+    $scope.countdown();
+   }, 1000);
+ };
+$scope.stop = function(){
+  $timeout.cancel(stopped);
+   }
+}]);
